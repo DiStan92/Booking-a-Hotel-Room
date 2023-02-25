e60430097c193b1f70ce04517a479c164fd71591
DiStan92 <79769027+DiStan92@users.noreply.github.com>
2023-02-25 15:26:24 +0300

    add env vars (#4)
    Co-authored-by: Dima Stantsel <dimastantse@gmail.com>

--- BookingHottelRoom/BookingHottelRoom/settings.py
+++ BookingHottelRoom/BookingHottelRoom/settings.py
@@ -1,4 +1,10 @@
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+# Import enviroment variables
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,12 +14,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-&mix9%7v$&v8^*+(u_(-vemp(#(rpj2v+)mi4a7%p*n^5sw!6i"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
