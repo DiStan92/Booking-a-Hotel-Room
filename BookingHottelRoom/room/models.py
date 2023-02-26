0d07a59f935f26fba094505a806b176e0cbcd0b4
Dima Stantsel <dimastantse@gmail.com>
2023-02-26 10:01:52 +0300

    create room models

--- BookingHottelRoom/room/models.py
+++ BookingHottelRoom/room/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Room(models.Model):
+    number = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    places = models.IntegerField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.pk} - {self.number}'
+
+    class Meta:
+        verbose_name = 'number'
+        verbose_name_plural = 'numbers'
