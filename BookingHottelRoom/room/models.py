80d124433a3352ec2bc9c4a44b693664260f0255
Dima Stantsel <dimastantse@gmail.com>
2023-02-26 15:06:59 +0300

    create room model

--- BookingHottelRoom/room/models.py
+++ BookingHottelRoom/room/models.py
@@ -4,8 +4,6 @@ class Room(models.Model):
     number = models.IntegerField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
     places = models.IntegerField()
-    date_created = models.DateTimeField(auto_now_add=True)
-    date_updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f'{self.pk} - {self.number}'
--- BookingHottelRoom/room/urls.py
+++ BookingHottelRoom/room/urls.py
@@ -1,10 +1,10 @@
 from django.urls import include, path, re_path
 from rest_framework.routers import DefaultRouter
 
-from .endpoints import RoomViewSet
+#from .endpoints import RoomViewSet
 
 router = DefaultRouter()
-router.register(r"room", RoomViewSet)
+#router.register(r"room", RoomViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
