9b1d2e88435f6799fa8cc9cc3c987436a8de1a4c
DiStan92 <79769027+DiStan92@users.noreply.github.com>
2023-02-26 15:13:01 +0300

    Create models room (#6)
    * create room_model migrations
    
    * create room models
    
    * create endpoint room model
    
    * update db
    
    * env.dev
    
    * create room model
    
    * update db
    
    ---------
    
    Co-authored-by: Dima Stantsel <dimastantse@gmail.com>

--- BookingHottelRoom/room/models.py
+++ BookingHottelRoom/room/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Room(models.Model):
+    number = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    places = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.pk} - {self.number}'
+
+    class Meta:
+        verbose_name = 'number'
+        verbose_name_plural = 'numbers'
