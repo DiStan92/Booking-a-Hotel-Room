c62f07977f36cb90dd8c7422acfdfffbce70865d
Dima Stantsel <dimastantse@gmail.com>
2023-03-01 11:00:45 +0300

    update  booking endpoint

--- BookingHottelRoom/users/endpoints.py
+++ BookingHottelRoom/users/endpoints.py
@@ -1,6 +1,8 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import permissions, status
 from rest_framework.generics import CreateAPIView, ListAPIView, ListCreateAPIView
 from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 
 from .models import Booking, User
 from .serializers import BookingSerializer, UserRegistrSerializer
@@ -23,10 +25,24 @@ class RegistrUserView(CreateAPIView):
             return Response(data)
 
 
-class BookingLCView(ListCreateAPIView):
+class BookingLCView(ListAPIView):
     serializer_class = BookingSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         guest = self.kwargs["id"]
         return Booking.objects.filter(guest_id=guest)
+
+
+class BookingListView(ListCreateAPIView):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["date_on", "date_off"]
+
+
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [permissions.IsAuthenticated]
--- BookingHottelRoom/users/urls.py
+++ BookingHottelRoom/users/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path, re_path
+from django.urls import include, path, re_path
+from rest_framework import routers
+from rest_framework.routers import DefaultRouter
 
-from .endpoints import BookingLCView, RegistrUserView
+from .endpoints import BookingLCView, BookingViewSet, RegistrUserView
+
+router = DefaultRouter()
+router.register("booking-viewset", BookingViewSet)
 
 urlpatterns = [
+    path("", include((router.urls, "booking"))),
     path("registr/", RegistrUserView.as_view(), name="registr"),
     re_path("^guest/(?P<id>.+)/booking", BookingLCView.as_view(), name="user-booking"),
 ]
