9890bbde16d84c53a8b4dbb155a6176d747037d4
DiStan92 <79769027+DiStan92@users.noreply.github.com>
2023-03-01 11:43:25 +0300

    update endpoint (#13)
    Co-authored-by: Dima Stantsel <dimastantse@gmail.com>

--- BookingHottelRoom/users/urls.py
+++ BookingHottelRoom/users/urls.py
@@ -1,14 +1,14 @@
 from django.urls import include, path, re_path
-from rest_framework import routers
 from rest_framework.routers import DefaultRouter
 
-from .endpoints import BookingLCView, BookingViewSet, RegistrUserView
+from .endpoints import BookingLCView, BookingListView, BookingViewSet, RegistrUserView
 
 router = DefaultRouter()
 router.register("booking-viewset", BookingViewSet)
 
 urlpatterns = [
     path("", include((router.urls, "booking"))),
+    path("booking/", BookingListView.as_view(), name="user-booking"),
     path("registr/", RegistrUserView.as_view(), name="registr"),
     re_path("^guest/(?P<id>.+)/booking", BookingLCView.as_view(), name="user-booking"),
 ]
